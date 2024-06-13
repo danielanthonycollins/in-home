c57c135b67a3495b666fb027b7b5616f1020bf8d
Daniel Collins <danielanthonycollins@gmail.com>
2024-06-13 15:28:09 +0000

    Settings

--- in_home/settings.py
+++ in_home/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['in-home-9aea5ddaebd0.herokuapp.com', '8000-danielanthonycol-inhome-ja1v5djvpcz.ws.codeinstitute-ide.net', 'localhost']
 

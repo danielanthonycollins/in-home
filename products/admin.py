be85b58ad669955e9a9150984967e6da2c22a597
Daniel Collins <danielanthonycollins@gmail.com>
2024-06-30 04:50:26 +0000

    Set up admin for Reviews

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
-
-# Register your models here.
+from .models import Product, Category, Review
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -20,5 +18,16 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'rating',
+        'details',
+        'submitted_by',
+        'date_submitted',
+    )
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Review, ReviewAdmin)

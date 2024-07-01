7e4e47905a6387f37f47296d2021059c6b68a929
Daniel Collins <danielanthonycollins@gmail.com>
2024-07-01 16:09:24 +0000

    Set up admin for contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,16 @@
+
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'full_name',
+        'email',
+        'subject',
+        'enquiry',
+        'date_submitted',
+    )
+
+admin.site.register(Contact, ContactAdmin)

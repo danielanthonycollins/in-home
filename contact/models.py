482b653ba6d145e1a6df3273e709f2cd498e80a5
Daniel Collins <danielanthonycollins@gmail.com>
2024-07-01 16:07:24 +0000

    Replace timestamp with date submitted

--- contact/models.py
+++ contact/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
-    full_name = models.CharField(max_length=50, null=True, blank=False)
-    email = models.EmailField(max_length=100, null=True, blank=False)
+    full_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(max_length=100, blank=False)
     subject = models.CharField(max_length=100, null=False, blank=False)
     enquiry = models.TextField()
     date_submitted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.email} - {self.timestamp.strftime('%d-%m-%Y')}"
+        return f"{self.email} - {self.date_submitted.strftime('%d-%m-%Y')}"

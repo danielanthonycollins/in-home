8f0fdef912669e86700684ba6a0a84508808f936
Daniel Collins <danielanthonycollins@gmail.com>
2024-07-01 15:15:42 +0000

    Create contact app and set up model

---
+++ contact/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Contact(models.Model):
+    full_name = models.CharField(max_length=50, null=True, blank=False)
+    email = models.EmailField(max_length=100, null=True, blank=False)
+    subject = models.CharField(max_length=100, null=False, blank=False)
+    enquiry = models.TextField()
+    date_submitted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.email} - {self.timestamp.strftime('%d-%m-%Y')}"

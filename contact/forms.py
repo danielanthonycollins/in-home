ee72749fa5d447230ea02923044aa9bada932fad
Daniel Collins <danielanthonycollins@gmail.com>
2024-07-02 14:25:49 +0000

    Style contact page and add contact details

--- contact/forms.py
+++ contact/forms.py
@@ -6,5 +6,11 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = ['full_name', 'email', 'subject', 'enquiry']
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            if field == 'enquiry':
+                self.fields[field].widget.attrs['class'] = 'extra-form-label-styling-tall'
+            else: 
+                self.fields[field].widget.attrs['class'] = 'extra-form-label-styling'

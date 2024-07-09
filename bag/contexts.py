6f05406b218640bc8014f86d4195989b8f739418
Daniel Collins <danielanthonycollins@gmail.com>
2024-07-09 13:11:51 +0000

    Pep8 compliant changes

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,8 +3,11 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def bag_contents(request):
-    """ Generates bag contents including pricing details and delivery costs. """
+    """
+    Generates bag contents including pricing details and delivery costs.
+    """
     bag_items = []
     total = 0
     product_count = 0
@@ -26,9 +29,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
